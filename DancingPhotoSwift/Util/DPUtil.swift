af2d8a05840681cc5f81f88746b053825974dbbe
hite <iamhite@gmail.com>
2018-07-23 17:32:48 +0800

    change: 强制用 Swift 4.0 编译 Layout、snapkit 两个库。因为他们对 4.2 都没有兼容

---
+++ DancingPhotoSwift/Util/DPUtil.swift
@@ -0,0 +1,13 @@
+//
+//  DPUtil.swift
+//  DancingPhotoSwift
+//
+//  Created by liang on 2018/7/20.
+//  Copyright © 2018 liang. All rights reserved.
+//
+
+import UIKit
+
+class DPUtil: NSObject {
+    
+}
---
+++ Pods/Target Support Files/Pods-DancingPhotoSwift/Pods-DancingPhotoSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DancingPhotoSwift : NSObject
+@end
+@implementation PodsDummy_Pods_DancingPhotoSwift
+@end
